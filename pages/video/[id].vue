720dd1e8aa80911f7a12556a6fa72a22fafc7e02
wkk <1037316703@qq.com>
2024-06-13 18:42:07 +0800

    mod  抽离组件

---
+++ pages/video/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <AppHeader />
+        <h2>视频页{{$route.params.id}}</h2>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const { params } = useRoute()
+console.log(params);
+
+
+</script>
+
+<style scoped></style>
